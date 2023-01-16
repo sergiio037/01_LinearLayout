0b246be1688ea2a9e756f51405ede567bedf2042
sergi <sergio2010moreno@gmail.com>
2023-01-16 20:27:05 +0100

    02

--- app/src/main/java/com/example/ejercicio03/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio03/MainActivity.kt
@@ -7,5 +7,8 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
